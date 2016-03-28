c0d573bd8064bcf534348b96ce4c0ef0b2d77bb8
Stefan Schneider <stefan.schneider@hpe.com>
2016-03-28 16:10:35 +0300

    Remove PATH environment variable dependency

--- bin/compile.bat
+++ bin/compile.bat
@@ -2,8 +2,10 @@
 set build_path=%1
 set cache_path=%2
 :: do whatever is needed for the application to run
+
 echo Copying buildpack scripts ...
-mkdir %build_path%\runner
-xcopy %~dp0\..\runner %build_path%\runner\ /E
+mkdir "%build_path%\runner"
+xcopy "%~dp0..\runner" "%build_path%\runner\" /E
+
 echo Done.
 exit /b 0
